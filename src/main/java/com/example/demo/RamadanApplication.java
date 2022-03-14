bcd7b4e7e8e447ed40dc59862283e9351436216c
AbdirahmanAbdulkadir <a.forabdi@hotmail.co.uk>
2022-03-14 10:44:56 +0000

    initial commit

---
+++ src/main/java/com/example/demo/RamadanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamadanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamadanApplication.class, args);
+	}
+
+}
